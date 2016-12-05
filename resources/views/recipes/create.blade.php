862e2cdca341d8f3b4ede6a063fed43ce47188eb
xEeefje <_eeef@live.nl>
2016-12-05 10:38:24 +0100

    hoofdletters veranderd

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -5,8 +5,8 @@
 	<h1>Schrijf een recept!</h1>
 
 	<form>
-		titel<input type="text" name=""><br>
-		bereidingswijze<input type="text" name=""><br>
+		Titel<input type="text" name=""><br>
+		Bereidingswijze<input type="text" name=""><br>
 		<input type="submit" name="">
 	</form>
 
