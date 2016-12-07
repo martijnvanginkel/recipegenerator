bd84657f4861263e42004280b542aea28b347f5d
deRaaf <timraaf@hotmail.com>
2016-12-07 14:03:12 +0100

    (Form overzichtelijk gemaakt)

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -2,54 +2,43 @@
 
 @section('content')
 
-	<h1>Recepten toevoegen</h1>
 
-	<form method="POST" action=" {{ route('recipes.store') }} ">
-		@if (count($errors) > 0)
-			    <div class="alert alert-danger">
-			        <ul>
-			            @foreach ($errors->all() as $error)
-			                <li>{{ $error }}</li>
-			            @endforeach
-			        </ul>
-			    </div>
-		@endif
-    <label for="">Afbeelding</label>
-    <input type="file" name="image" accept="image/*">
-		<label name="title">Titel</label>
-		<br>
-		<input type="text" name="titel" id="titel">
-		<br>
-		<label name="ingredienten" for="">Ingrediënten:</label>
-		<br>
-		<input type="text" name="ingredienten" id="ingredienten" value="">
-		<br>
-		<label name="bereidingswijze">Bereidingswijze:</label>
-		<br>
-		<input type="text" name="bereidingswijze" id="bereidingswijze">
-		<br>
-		<label name="voedingswaarde" for="">Voedingswaarde:</label>
-		<br>
-		<input name="voedingswaarde" id="voedingswaarde">
-		<br>
-		<label name="allergieen" for="">Allergieën:</label>
-		<br>
-		<input type="checkbox" name="allergy_1" value="">
-		<input type="checkbox" name="allergy_2" value="">
-		<input type="checkbox" name="allergy_3" value="">
-		<input type="checkbox" name="allergy_4" value="">
-		<input type="checkbox" name="allergy_5" value="">
-		<input type="checkbox" name="allergy_6" value="">
-		<br>
-		<label name="dieet" for="">Dieet:</label>
-		<br>
+  <div id="container">
+  	<form id="form_new_recipe" method="POST" action=" {{ route('recipes.store') }} ">
+  		@if (count($errors) > 0)
+  			    <div class="alert alert-danger">
+  			        <ul>
+  			            @foreach ($errors->all() as $error)
+  			                <li>{{ $error }}</li>
+  			            @endforeach
+  			        </ul>
+  			    </div>
+  		@endif
+      <h1>Recepten toevoegen</h1>
+      <label for="">Afbeelding</label>
+      <input type="file" name="image" accept="image/*">
+  		<label name="title">Titel</label>
+  		<input type="text" name="titel" id="titel">
+  		<label name="ingredienten" for="">Ingrediënten:</label>
+  		<input type="text" name="ingredienten" id="ingredienten" value="">
+  		<label name="bereidingswijze">Bereidingswijze:</label>
+  		<input type="text" name="bereidingswijze" id="bereidingswijze">
+  		<label name="voedingswaarde" for="">Voedingswaarde:</label>
+  		<input name="voedingswaarde" id="voedingswaarde">
+  		<label name="allergieen" for="">Allergieën:</label>
+  		<input type="checkbox" name="allergy_1" value="">
+  		<input type="checkbox" name="allergy_2" value="">
+  		<input type="checkbox" name="allergy_3" value="">
+  		<input type="checkbox" name="allergy_4" value="">
+  		<input type="checkbox" name="allergy_5" value="">
+  		<input type="checkbox" name="allergy_6" value="">
+  		<label name="dieet" for="">Dieet:</label>
 
-		<input type="checkbox" name="vegan" value="">
-		<input type="checkbox" name="vegetarian" value="">
-		<br>
-		<input type="submit" value="Voeg recept toe">
+  		<input type="checkbox" name="vegan" value="">
+  		<input type="checkbox" name="vegetarian" value="">
+  		<input type="submit" value="Voeg recept toe">
 
-		<input type="hidden" name="_token" value="{{ Session::token() }}">
-	</form>
-
+  		<input type="hidden" name="_token" value="{{ Session::token() }}">
+  	</form>
+  </div>
 @endsection
---
+++ resources/views/vendor/index.blade.php
@@ -0,0 +1,17 @@
+@extends('main')
+
+@section('content')
+  <section id="generator">
+    <input type="text" name="" value="">
+    <button type="button" name="button"></button>
+  </section>
+  <section id="recept">
+    <img src="" alt="" />
+    <ol id="ingredienten">
+      <!-- vanuit PHP -->
+    </ol>
+    <div id="stappenplan">
+      <!-- vanuit PHP -->
+    </div>
+  </section>
+@endsection
