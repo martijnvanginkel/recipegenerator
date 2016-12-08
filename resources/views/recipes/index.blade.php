0fff36f8531ab0d544cb7265301dcb612abb179f
Martijn van Ginkel <Martijn@mbp-van-martijn.nhl.nl>
2016-12-08 13:27:02 +0100

    substr's gemaakt en wijzig en bekijk knop gemaakt

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -19,15 +19,19 @@
 			<th>ingredienten</th>
 			<th>bereidingswijze</th>
 			<th>voedingwaarde</th>
+			<th></th>
+			<th></th>
 		</tr>
 
 			@foreach ($recipes as $recipe)
 				<tr>
 					<td>{{ $recipe->id }}</td>
-					<td>{{ $recipe->titel }}</td>
-					<td>{{ $recipe->ingredienten }}</td>
-					<td>{{ $recipe->bereidingswijze }}</td>
-					<td>{{ $recipe->voedingswaarde }}</td>
+					<td>{{ substr($recipe->titel, 0, 30) }} {{ strlen($recipe->titel) > 30 ? "..." : "" }}</td>
+					<td>{{ substr($recipe->ingredienten, 0, 30) }} {{ strlen($recipe->ingredienten) > 30 ? "..." : "" }} </td>
+					<td>{{ substr($recipe->bereidingswijze, 0, 30) }} {{ strlen($recipe->bereidingswijze) > 30 ? "..." : "" }}</td>
+					<td>{{ substr($recipe->voedingwaarde, 0, 30) }} {{ strlen($recipe->voedingwaarde) > 30 ? "..." : "" }}</td>
+					<td><a href=" {{ route('recipes.show', $recipe->id) }} ">Bekijk</a>  </td>
+					<td><a href=" {{ route('recipes.edit', $recipe->id) }} ">Wijzig</a></td>
 				</tr>
 			@endforeach
 	</table>
