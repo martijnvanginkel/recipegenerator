20ddea5dc4984f5b63b1b5ded1f1bf0ef49380e3
Martijn van Ginkel <Martijn@mbp-van-martijn.nhl.nl>
2016-12-08 14:17:08 +0100

    dingen in de functie

---
+++ app/Http/Controllers/GenerateController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Recipe;
+use Illuminate\Support\Facades\Input;
+
+class GenerateController extends Controller
+{
+
+	public function generate(){
+
+		$clicked =  Input::get('genereer');
+
+		if ($clicked) {
+
+			$recipeAmount = Recipe::count();
+	        $randomNumber = rand(1, $recipeAmount);
+	        $id = $randomNumber;
+	        $recipe = Recipe::find($id);
+
+        	return view('/home')->with('recipe', $recipe)->with('clicked', $clicked);
+		}else {
+			return view('/home')->with('clicked', $clicked);
+		}
+
+	}
+
+}
