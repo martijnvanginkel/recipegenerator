c8b4fe1933ff35177ec33a779c4cf98c509fb5df
Martijn van Ginkel <Martijn@mbp-van-martijn.nhl.nl>
2016-12-16 14:06:04 +0100

    dieeten zijn nu bij het recept te zien

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
 
+	<img id="image_recipe" src="{{ asset('img/' . $recipe->image) }}" alt="" height="250px" width="900px"/>
 	
 	<h1> {{ $recipe->titel }} </h1>
 
-	<h2>Ingrediënten</h2>
+	<h1>Ingrediënten</h1>
 	<p> {{ $recipe->ingredienten }} </p><br>
 
-	<h2>Bereidingswijze</h2>
+	<h1>Bereidingswijze</h1>
 	<p> {{ $recipe->bereidingswijze }} </p><br>
 
-	<h2>Voedingswaarde</h2>
+	<h1>Voedingswaarde</h1>
 	<p> {{ $recipe->voedingswaarde }} </p><br>
+
+	<h1>Dieeten</h1>
+	@foreach($recipe->diets as $diet)
+		<span>{{ $diet->titel }}</span>
+
+	@endforeach
+
 	
 	<a href=" {{ route('recipes.index', $recipe->id) }} ">Terug</a>
 
