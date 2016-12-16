67ff5a7eb54d39305b1dbeba9d37e9d0fe782d32
Lars van Urk <larsvanurk@Larss-MacBook-Pro.local>
2016-12-16 12:23:16 +0100

    Automatische resize van tabellen in recipe create

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -3,56 +3,52 @@
 @section('content')
 
 
-
   <div id="container">
-  	<form id="form_new_recipe" method="POST" action=" {{ route('recipes.store') }} " enctype="multipart/form-data">
+    <form id="form_new_recipe" method="POST" action=" {{ route('recipes.store') }} " enctype="multipart/form-data">
 
-  		@if (count($errors) > 0)
-  			    <div class="alert alert-danger">
-  			        <ul>
-  			            @foreach ($errors->all() as $error)
-  			                <li>{{ $error }}</li>
-  			            @endforeach
-  			        </ul>
-  			    </div>
-  		@endif
+      @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+      @endif
       <h1>Recepten toevoegen</h1>
-      <label for="">Afbeelding</label>
+      <label for="">Afbeelding:</label>
       <input type="file" name="image" accept="image/*"></textarea>
-  		<label name="title">Titel:</label>
-  		<input type="text" name="titel" id="titel"></textarea>
-  		<label name="ingredienten" for="">Ingrediënten:</label>
-  		<textarea type="text" name="ingredienten" id="ingredienten" value=""></textarea>
-  		<label name="bereidingswijze">Bereidingswijze:</label>
-  		<textarea type="text" name="bereidingswijze" id="bereidingswijze"></textarea>
-  		<label name="voedingswaarde" for="">Voedingswaarde:</label>
-  		<textarea name="voedingswaarde" id="voedingswaarde"></textarea>
-  		<!-- <label name="allergieen" for="">Allergieën:</label>
-  		<input type="checkbox" name="allergy_1" value="">
-  		<input type="checkbox" name="allergy_2" value="">
-  		<input type="checkbox" name="allergy_3" value="">
-  		<input type="checkbox" name="allergy_4" value="">
-  		<input type="checkbox" name="allergy_5" value="">
-  		<input type="checkbox" name="allergy_6" value=""> -->
+      <label name="title">Titel:</label>
+      <input type="text" name="titel" id="titel"></textarea>
+      <label name="ingredienten" for="">Ingrediënten:</label>
+      <textarea type="text" name="ingredienten" id="ingredienten" value=""></textarea>
+      <label name="bereidingswijze">Bereidingswijze:</label>
+      <textarea type="text" name="bereidingswijze" id="bereidingswijze"></textarea>
+      <label name="voedingswaarde" for="">Voedingswaarde:</label>
+      <textarea name="voedingswaarde" id="voedingswaarde"></textarea>
 
-  		<label name="diets" for="diets">Dieet:</label>
+      <!-- <label name="allergieen" for="">Allergieën:</label>
+      <input type="checkbox" name="allergy_1" value="">
+      <input type="checkbox" name="allergy_2" value="">
+      <input type="checkbox" name="allergy_3" value="">
+      <input type="checkbox" name="allergy_4" value="">
+      <input type="checkbox" name="allergy_5" value="">
+      <input type="checkbox" name="allergy_6" value=""> -->
 
-      <select class="form-control select2-multi" name="diets[]" multiple="multiple">
-        @foreach($diets as $diet)
-          <option value="{{ $diet->id }}"> {{ $diet->titel }} </option>
-        @endforeach
-      </select>
+      <label name="diet" for="">Dieet:</label>
+      @foreach($diets as $diet)
+      <input type="checkbox" id="{{$diet->titel}}" value="{{ $diet->id }}" ><label for="{{$diet->titel}}">{{$diet->titel}}</label>  
 
-  		<input type="submit" value="Voeg recept toe">
 
-  		<input type="hidden" name="_token" value="{{ Session::token() }}">
-  	</form>
+<!-- <input type="checkbox" id="vegetarisch" />
+      <label for="vegetarisch">Vegetarisch</label> -->
+
+
+  @endforeach
+
+      <input type="submit" value="Voeg recept toe">
+
+      <input type="hidden" name="_token" value="{{ Session::token() }}">
+    </form>
   </div>
-
-
-
-<script type="text/javascript">
-  $('.select2-multi').select2();
-</script>
-
 @endsection
