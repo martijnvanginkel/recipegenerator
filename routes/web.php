bbd5026b220c1b6b418df34a760ea8ce72a3ae01
Martijn van Ginkel <Martijn@mbp-van-martijn.nhl.nl>
2016-12-21 09:59:54 +0100

    recipe-favorite link gewijzigd naar users.index

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ Route::get('/profile', 'PagesController@getProfile');
 Route::resource('recipes', 'RecipeController');
 Route::resource('diets', 'DietController', ['except' => ['create']]);
 Route::resource('users', 'UserController');
-Route::resource('favorites', 'FavoriteController');
+
 
 Route::get('/home', 'RecipeController@generate')->name('generate');
 Route::get('/home', 'RecipeController@generate')->name('recipe-generate');
 Route::post('/home', 'RecipeController@generate')->name('recipe-generate');
-Route::post('/home', 'RecipeController@favorite')->name('recipe-favorite');
+Route::post('/users.index', 'RecipeController@favorite')->name('recipe-favorite');
 //Route::post('/users', 'GenerateController@store')->name('generate-store');
 
 Auth::routes();
