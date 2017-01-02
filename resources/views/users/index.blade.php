d481586f12606643818255902483ce3d57fdb7cc
deRaaf <timraaf@hotmail.com>
2017-01-02 12:02:53 +0100

    (Edit user gestijld)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,64 +3,64 @@
 @section('content')
 
 	<header>
+
 		<a href="/home"><img id="logo" src={{asset('img/Sjef_logo.png')}} alt="De Sjef Logo"></a>
-		<h1>Welkom op je profiel, <em>{{ $user->name }}</em>! </h1>
-		<a href=" {{ route('users.edit', $user->id) }} ">Hier kan je je instellingen wijzigen</a>
+		<div class="wrapper">
+			<h1>Welkom op je profiel, <em>{{ $user->name }}</em>! </h1>
+			<!-- <a href=" {{ route('users.edit', $user->id) }} ">Hier kan je je instellingen wijzigen</a> -->
 
+	    <table class="table table-bordered">
+
+	    <h3>Dit zijn jouw dieeten:</h3>
+
+	      @foreach ($user->diets as $diet)
+	        <tr>
+	          <td>{{ $diet->titel }}</td>
+	          <td>
+
+	    <form method="POST" action="{{ route('destroy-diet', $diet->id) }}">
+	      <input type="submit" value="Verwijder">
+	      <input type="hidden" name="_token" value="{{ Session::token() }}">
+	     {{ method_field('DELETE') }}
+	    </form>﻿
+
+	          </td>
+	        </tr>
+	      @endforeach
+	  </table>
+
+	    <form id="form_new_recipe" method="POST" action=" {{ route('users.store') }} " enctype="multipart/form-data">
+
+	      @if (count($errors) > 0)
+	            <div class="alert alert-danger">
+	                <ul>
+	                    @foreach ($errors->all() as $error)
+	                        <li>{{ $error }}</li>
+	                    @endforeach
+	                </ul>
+	            </div>
+	      @endif
+
+	    
+	    <select class="form-control" name="diet_id">
+
+	    <option selected disabled>Selecteer een dieet</option>
+
+	      @foreach($diets as $diet)
+
+	          <option value="{{ $diet->id }}"> {{ $diet->titel }} </option>
+
+	      @endforeach
+
+	   </select>
+
+	      <input type="submit" value="Voeg toe">
+
+	      <input type="hidden" name="_token" value="{{ Session::token() }}">
+
+	    </form>
+		</div>
 	</header>
-
-    <table class="table table-bordered">
-
-    <h1>Dit zijn jouw dieeten:</h1>
-
-      @foreach ($user->diets as $diet)
-        <tr>
-          <td>{{ $diet->titel }}</td>
-          <td>
-
-    <form method="POST" action="{{ route('destroy-diet', $diet->id) }}">
-      <input type="submit" value="Verwijder">
-      <input type="hidden" name="_token" value="{{ Session::token() }}">
-     {{ method_field('DELETE') }}
-    </form>﻿ 
-
-          </td>
-        </tr>
-      @endforeach
-  </table>
-
-    <form id="form_new_recipe" method="POST" action=" {{ route('users.store') }} " enctype="multipart/form-data">
-
-      @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-      @endif
-
-    <label name="diets" for="diets">Dieeten:</label>
-    <select class="form-control" name="diet_id">
-
-    <option selected disabled>Selecteer een dieet</option>
-
-      @foreach($diets as $diet)
-     
-          <option value="{{ $diet->id }}"> {{ $diet->titel }} </option>
-          
-      @endforeach
-
-   </select>
-
-      <input type="submit" value="Voeg toe">
-
-      <input type="hidden" name="_token" value="{{ Session::token() }}">
-
-    </form>
-
-
 
   <section id="favorieten">
     <h1>Favorieten</h1>
