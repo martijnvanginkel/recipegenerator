23e7f95bde9cad7eb8f639974688081588ac3087
Tim de Raaf <Tim de Raaf>
2017-01-04 22:07:11 +0100

    (Indeling (ID's) aangepast naar de zelfde als favorieten)

--- resources/views/pages/history.blade.php
+++ resources/views/pages/history.blade.php
@@ -1,22 +1,25 @@
 @extends('main')
 
 @section('content')
-
+<section id="container_recept_fav">
 	<img id="image_recipe" src="{{ asset('img/' . $recipe->image) }}" alt="" height="250px" width="900px"/>
-	
-	<h1> {{ $recipe->titel }} </h1>
-
-	<h1>Ingrediënten</h1>
-	<p> {{ $recipe->ingredienten }} </p><br>
-
-	<h1>Bereidingswijze</h1>
-	<p> {{ $recipe->bereidingswijze }} </p><br>
-
-	<h1>Voedingswaarde</h1>
-	<p> {{ $recipe->voedingswaarde }} </p><br>
-
-	
 	<a href=" {{ route('users.index') }} ">Terug</a>
-
+	<h1>{{ $recipe->titel }}</h1>
+	<ul id="ingredients">
+		<h3>Ingrediënten</h3>
+		<!-- vanuit PHP -->
+		<li>{{ $recipe->ingredienten }}</li>
+	</ul>
+	<div id="steps">
+		<h3>Bereidingswijze</h3>
+		<!-- vanuit PHP -->
+		<p>{{ $recipe->bereidingswijze }}</p>
+	</div>
+	<div id="nutritional_values">
+		<h3>Voedingswaarden</h3>
+		<!-- vanuit PHP -->
+		<p>{{ $recipe->voedingswaarde }}</p>
+	</div>
+</section>
 
 @endsection
