55bcc67012bccc6b8149e3ad926792dc82397823
xEeefje <_eeef@live.nl>
2017-01-09 09:52:20 +0100

    comment controller gemaakt

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -9,6 +9,7 @@ use App\Recipe;
 
 class CommentsController extends Controller
 {
+    //plaatsen van reactie op home pagina
     public function store(Request $request, Recipe $recipe)
     {
         $user = Auth::user();
@@ -28,5 +29,37 @@ class CommentsController extends Controller
         $recipe->comments()->save($comment);
 
         return back();
+    }
+
+    //bewerken reactie op user pagina
+    public function edit($id)
+    {
+        $comment = Comment::find($id);
+
+        return view('comments.edit')->with('comment', $comment);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,
+        [
+            'comment' => 'required',
+        ]);
+
+        $comment = Comment::find($id);
+
+        $comment->comment = $request->comment;
+
+        $recipe->comments()->save($comment);
+
+        return redirect()->route('users.index');
+    }
+
+    //verwijderen van reactie op user pagina
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $comment->delete();
+        return redirect()->route('users.index');
     }
 }
