daf2e6a8b467d41cb2330ebde7ca737adc75add7
xEeefje <_eeef@live.nl>
2017-01-09 09:53:58 +0100

    comment edit toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,7 @@ Route::resource('users', 'UserController', ['except' => ['show']]);
 Route::get('/users/favorites/{id}', 'UserController@favorite')->name('favorite-recipes');
 Route::get('/users/history/{id}', 'UserController@history')->name('history-recipes');
 
-
 Route::get('/users.index', 'RecipeController@history')->name('history');
-
 
 Route::get('/home', 'RecipeController@generate')->name('generate');
 Route::get('/home', 'RecipeController@generate')->name('recipe-generate');
@@ -33,6 +31,10 @@ Route::post('/users.index', 'RecipeController@favorite')->name('recipe-favorite'
 Route::delete('/users.index/{id}', 'UserController@destroyDiet')->name('destroy-diet');
 
 Route::post('/recipes/{recipe}/comments', 'CommentsController@store');
+Route::delete('/recipes/{recipe}', 'CommentsController@destroy')->name('destroy-comment');
+
+Route::get('/users/{comments}', 'CommentsController@edit')->name('comments-edit');
+Route::post('/users/{comments}', 'CommentsController@update')->name('comments-update');
 
 Auth::routes();
 
