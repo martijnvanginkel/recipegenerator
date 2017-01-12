57c8e5a4629e04612904b06ba598abc9380926fd
xEeefje <_eeef@live.nl>
2017-01-12 13:37:49 +0100

    foreach voor allergieën en diëten toegevoegd

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -29,14 +29,22 @@
     <label for="voedingswaarde">Voedingswaarden:</label>
     <textarea placeholder="Typ hier de voedingswaarden van dit gerecht" type="text" id="voedingswaarde" name="voedingswaarde" rows="10">{{ $recipe->voedingswaarde }}</textarea>
 
+  <label name="foodrestrictions[]" for="foodrestrictions">Dit recept past binnen de volgende allergieën:</label>
+    <ul>
+  @foreach($foodrestrictions->where('allergy', true) as $foodrestriction)
+      <li> <input name="foodrestrictions[]" type="checkbox" id="{{$foodrestriction->title}}" value="{{ $foodrestriction->id }}" ><label for="{{$foodrestriction->title}}" >
+      
+      {{$foodrestriction->title}}</label> </li>
+  @endforeach
+    </ul>
+
     <label name="foodrestrictions[]" for="foodrestrictions">Dit recept past binnen de volgende diëten:</label>
 
-{{--     <ul>
+    <ul>
   @foreach($foodrestrictions->where('diet', true) as $foodrestriction)
       <li> <input name="foodrestrictions[]" type="checkbox" id="{{$foodrestriction->title}}" value="{{ $foodrestriction->id }}" ><label for="{{$foodrestriction->title}}" >{{$foodrestriction->title}}</label> </li>
-      <li> <input name="foodrestrictions[]" type="checkbox" {{$foodrestriction}}  id="{{$foodrestriction->title}}" value="{{ $foodrestriction->id }}" ><label for="{{$foodrestriction->title}}" >{{$foodrestriction->title}}</label> </li>
   @endforeach
-    </ul> --}}
+    </ul>
 
 	<button id="save-button" type="submit">Opslaan</button>
     
